aaeeae21b0dd17d567fdd1134e95a9628d3f288b
Ace <akhmadfauzy40@gmail.com>
2025-09-30 18:16:10 +0700

    feat: all featues done

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -249,11 +249,11 @@
                     <!--begin::Logo-->
                     <div class="app-sidebar-logo px-6" id="kt_app_sidebar_logo">
                         <!--begin::Logo image-->
-                        <a href="index.html">
-                            <img alt="Logo" src="assets/media/logos/default-dark.svg"
-                                class="h-25px app-sidebar-logo-default" />
-                            <img alt="Logo" src="assets/media/logos/default-small.svg"
-                                class="h-20px app-sidebar-logo-minimize" />
+                        <a href="/">
+                            <img alt="Logo" src="assets/media/logos/logo-with-text.png"
+                                class="h-40px app-sidebar-logo-default" />
+                            <img alt="Logo" src="assets/media/logos/logo-only.png"
+                                class="h-30px app-sidebar-logo-minimize" />
                         </a>
                         <div id="kt_app_sidebar_toggle"
                             class="app-sidebar-toggle btn btn-icon btn-shadow btn-sm btn-color-muted btn-active-color-primary h-30px w-30px position-absolute top-50 start-100 translate-middle rotate"
